cb9eb3270e03093b0b2c3c788cbc9fe794dc5337
15005415 <jeremy.lafont@etudiant.univ-rennes1.fr>
2020-05-25 15:06:19 +0200

    Sauvegarde des données dans convergence

--- openvidu-call-front/src/app/tp-creation/tp-creation.component.ts
+++ openvidu-call-front/src/app/tp-creation/tp-creation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {Convergence} from '@convergence/convergence';
 
 @Component({
   selector: 'app-tp-creation',
@@ -11,5 +13,32 @@ export class TpCreationComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  createModel(nomTP: string, contenuTP: string): void {
+
+	const username = 'User-' + Math.round(Math.random() * 10000);
+
+		Convergence.connectAnonymously(environment.CONVERGENCE_URL, username)
+			.then(d => {
+			const domain: any = d;
+			// Now open the model, creating it using the initial data if it does not exist.
+			return domain.models().openAutoCreate({
+		id: 'my-model-id',
+		collection: 'myTP',
+		ephemeral: true,
+		data: {
+			'nomDuTp': nomTP,
+			'contenuTP': contenuTP
+		}
+		})
+		.then((model) => {
+			console.log('model open');
+			// use the model
+		})
+			.catch((error) => console.log('Could not open the model', error));
+		});
+
+
+}
 
 }
