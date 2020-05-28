cdb49ee19e3f63bcdfb018d2d037daf646176f3f
Lafont Jeremy <jeremy.lafont@etudiant.univ-rennes1.fr>
2020-05-28 14:49:09 +0200

    Push avec method editor

--- openvidu-call-front/src/app/myeditor/myeditor.component.ts
+++ openvidu-call-front/src/app/myeditor/myeditor.component.ts
@@ -20,6 +20,66 @@ export class MyeditorComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges){
+    this.editor(this.numeroEtudiant);
+  }
+
+
+  editor(numeroEtudiant: string){
+    (window as any).require.config({
+      paths: {
+        vs: '/vs',
+        Convergence: '/convergence.amd.js',
+        ConvergenceColorAssigner: '/color-assigner.js',
+        MonacoConvergenceAdapter: './monaco-adapter.js',
+        rxjs: 'https://unpkg.com/rxjs@6.4.0/bundles/rxjs.umd.js'
+      }
+    });
+
+    // Connect to the domain.  See ../config.js for the connection settings.
+
+    const username = 'User-' + Math.round(Math.random() * 10000);
+
+    (window as any).define(
+      'namedefine',
+      ['vs/editor/editor.main', 'Convergence', 'MonacoConvergenceAdapter'],
+      (_, Convergence, MonacoConvergenceAdapter) => {
+        //
+        // Create the target editor where events will be played into.
+        //
+
+
+        const modelId = 'tptest_' + numeroEtudiant ;
+        Convergence.connectAnonymously(environment.CONVERGENCE_URL, username)
+          .then(d => {
+            const domain: any = d;
+            // Now open the model, creating it using the initial data if it does not exist.
+            return domain.models().openAutoCreate({
+              collection: 'tptest',
+              id: modelId,
+              data: {
+                contenuTP: 'default declare var a;'
+              }
+            });
+          })
+          .then((model) => {
+            const defaultEditorContents = 'default declare var a = 4+5;';
+            const editor = monaco.editor.create(this.editorRef.nativeElement, {
+              value: defaultEditorContents,
+              theme: 'vs-dark',
+              language: 'javascript'
+            });
+
+            const adapter = new MonacoConvergenceAdapter(editor, model.elementAt('contenuTP'));
+            adapter.bind();
+          })
+          .catch(error => {
+            console.error('Could not open model ', error);
+          });
+      });
+
+  }
+
   ngAfterViewInit(): void {
 	(window as any).require.config({
 		paths: {
@@ -30,6 +90,8 @@ export class MyeditorComponent implements OnInit, AfterViewInit {
 		rxjs: 'https://unpkg.com/rxjs@6.4.0/bundles/rxjs.umd.js'
 		}
 	});
+
+
 
 	// Connect to the domain.  See ../config.js for the connection settings.
 
