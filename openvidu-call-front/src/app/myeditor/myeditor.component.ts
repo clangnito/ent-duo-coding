6883f27ab081d9455b44ec775f00db5eec53351c
Michael WOLOU <mwolou@gmail.com>
2020-05-31 17:10:49 +0200

    Contenu de l'editeur en fonction de l'etudiant réglé

--- openvidu-call-front/src/app/myeditor/myeditor.component.ts
+++ openvidu-call-front/src/app/myeditor/myeditor.component.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {MonacoConvergenceAdapter} from '../../MonacoConvergenceAdapter';
 
 
 declare const monaco: any;
@@ -11,9 +12,9 @@ declare const monaco: any;
 export class MyeditorComponent implements OnInit, AfterViewInit {
   @ViewChild('editor') editorRef: ElementRef;
   username = 'User-' + Math.round(Math.random() * 10000);
-
-  defaultEditorContents = 'declare var a = 4+5;gyilgygyg';
   editor: any;
+  currentModel: any;
+  adapter: any;
 
   constructor() {
 	(window as any).require.config({
@@ -21,72 +22,29 @@ export class MyeditorComponent implements OnInit, AfterViewInit {
 		vs: '/vs',
 		Convergence: '/convergence.amd.js',
 		ConvergenceColorAssigner: '/color-assigner.js',
-		MonacoConvergenceAdapter: './monaco-adapter.js',
 		rxjs: 'https://unpkg.com/rxjs@6.4.0/bundles/rxjs.umd.js'
 		}
 	});
   }
 
   ngOnInit(): void {
-
-	/*
-    (window as any).define(
-      'monaco-example2',
-      ['vs/editor/editor.main', 'MonacoConvergenceAdapter'],
-      (_, MonacoConvergenceAdapter) => {
-      //
-      // Create the target editor where events will be played into.
-      //
-      this.editor = monaco.editor.create(this.editorRef.nativeElement, {
-        value: this.defaultEditorContents,
-        theme: 'vs-dark',
-        language: 'javascript'
-      });
-  /*
-      Convergence.connectAnonymously(environment.CONVERGENCE_URL, this.username)
-      .then(d => {
-        const domain: any = d;
-        // Now open the model, creating it using the initial data if it does not exist.
-        return domain.models().openAutoCreate({
-        collection: 'example-monaco2',
-        id: 'Id-12',
-        data: {
-          text: this.defaultEditorContents
-        }
-        });
-      })
-      .then((model) => {
-        const adapter = new MonacoConvergenceAdapter(this.editor, model.elementAt('text'));
-        adapter.bind();
-        // (window as any).exampleLoaded();
-      })
-      .catch(error => {
-        console.error('Could not open model ', error);
-      });
-
-      });
-      // this.reloadViewContent();
-
-   */
   }
 
   ngAfterViewInit(): void {
   }
 
   reloadViewContent(domain: any, currentModelId: any): void {
-	// currentModelId = 'Id-12';
-	(window as any).define(
-	  currentModelId,
-		['vs/editor/editor.main', 'MonacoConvergenceAdapter'],
-		(_, MonacoConvergenceAdapter) => {
-
+	if (this.currentModel) {
+	  this.currentModel.close();
+	}
 		domain.models()
 			.open(currentModelId)
 			.then((model) => {
-		const value = model.elementAt('text').value();
-		console.log('model open', value);
+			  this.currentModel = model;
+		const value = this.currentModel.elementAt('text').value();
+		console.log('model open : Value ===> ', value);
 		if (this.editor) {
-			this.editor.setValue(value);
+		this.editor.setValue(value);
 		} else {
 			this.editor = monaco.editor.create(this.editorRef.nativeElement, {
 			value: value,
@@ -95,12 +53,10 @@ export class MyeditorComponent implements OnInit, AfterViewInit {
 			});
 		}
 
-
-			const adapter = new MonacoConvergenceAdapter(this.editor, model.elementAt('text'));
-			adapter.bind();
+			this.adapter = new MonacoConvergenceAdapter(this.editor, this.currentModel.elementAt('text'));
+			this.adapter.bind();
 			}).catch((error) => {
 			console.log('Could not open the model', error);
-		});
 		});
   }
 
